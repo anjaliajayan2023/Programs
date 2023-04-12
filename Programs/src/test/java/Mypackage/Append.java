792f10b8b88ba151ee02f6a67a384c72486c7051
anjaliajayan2023 <125172233+anjaliajayan2023@users.noreply.github.com>
2023-04-12 08:22:01 +0530

    First Commit

---
+++ Programs/src/test/java/Mypackage/Append.java
@@ -0,0 +1,12 @@
+package Mypackage;
+
+public class Append {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuffer sb=new StringBuffer("Hello");
+sb.append("World");
+System.out.println(sb);
+	}
+
+}
---
+++ Programs/src/test/java/Mypackage/MethodOverloadingExample.java
@@ -0,0 +1,15 @@
+package Mypackage;
+
+public class MethodOverloadingExample {
+	
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	public static int add(int a,int b,int c) {
+		return a+b+c;
+	}
+}
+	
+	
+
+	
